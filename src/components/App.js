af2c8787da424c5ddbbd04fc077115830bc60661
Ghimirevivek <stephengmre@gmail.com>
2023-03-25 15:00:25 +0530

    message

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import '../styles/App.css';
-import { UserProfile } from './UserProfile';
+import React, { createContext } from 'react'
+import '../styles/App.css'
+import { UserProfile } from './UserProfile'
 
-
+export const UserContext = createContext()
 const App = () => {
-
+  const user = { name: 'Newton', age: 3 }
   return (
     <div id="main">
-      
+      <UserContext.Provider value={user}>
         <UserProfile />
-      
+      </UserContext.Provider>
     </div>
   )
 }
 
-
-export default App;
-export {UserContext}
+export default App
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,13 @@
 import { useContext } from 'react'
 import React from 'react'
-import { UserContext } from "./App"
+import { UserContext } from './App'
 
-const UserProfile = () =>{
-
+export const UserProfile = () => {
+  const user = useContext(UserContext)
+  return (
+    <>
+      <h1 id="name">Name:-{user.name}</h1>
+      <h1 id="age">Age:-{user.age}</h1>
+    </>
+  )
 }
-
-export {UserProfile}
